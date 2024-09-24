f9de5a3f3b7d9fb9227b7c676782843dc7b690c3
JMHEIG <joyce.maiamba@heig-vd.ch>
2024-09-24 15:17:09 +0200

    add code calculator

--- main.cpp
+++ main.cpp
@@ -7,12 +7,38 @@
  */
  
  // -------------------------------------------------- include files for program
-
+#include <iostream>
 
 // -------------------------------------------------------------------  program
+using namespace std;
 
 int main() 
 {
+   float poids, taille, imc;
 
-	return 1;
+	// Demande du poids et de la taille
+	
+	cout << "Entrez votre poids (en kg) : ";
+	cin >> poids;
+	cout << "Entrez votre taille (en metres) : ";
+	cin >> taille;
+	
+	// Calcul de l'IMC
+	imc = poids / (taille * taille);
+
+	// Affichage de l'IMC
+	cout << "Votre IMC est : " << imc << endl;
+
+	// Interprétation de l'IMC
+	if (imc < 18.5) {
+		cout << "Vous avez un poids insuffisant." << endl;
+	} else if (imc < 25) {
+		cout << "Vous avez un poids normal." << endl;
+	} else if (imc < 30) {
+		cout << "Vous êtes en surpoids." << endl;
+	} else {
+		cout << "Vous êtes en situation d'obésité." << endl;
+	}
+
+	return EXIT_SUCCESS;
 }
